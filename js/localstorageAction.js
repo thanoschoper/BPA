61b12ef0673b2f3c6c48233b88abf8a80f174e62
Thomas <24ThomasL@mydacc.org>
2024-01-09 12:50:04 -0500

    localstorage

---
+++ js/localstorageAction.js
@@ -0,0 +1,14 @@
+const nameData = localStorage.getItem("name");
+        const emailData = localStorage.getItem("email");
+        const addressData = localStorage.getItem("address");
+
+        
+        console.log("Name Object:", nameData);
+        console.log("Email Object:", emailData);
+        console.log("Address Object:", addressData);
+
+         
+        
+        document.getElementById('name').innerText = nameData;
+        document.getElementById('email').innerText = emailData;
+        document.getElementById('address').innerText = addressData;
